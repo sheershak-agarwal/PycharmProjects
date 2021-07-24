cbe0ae932e37b948f49ee63392c61277d5463783
Sheershak Agarwal <sheershak.agarwal@wildstar.tech>
2021-07-24 17:01:20 -0400

    updated code

--- ParkAndMeasure/robot_arm.py
+++ ParkAndMeasure/robot_arm.py
@@ -3,7 +3,7 @@ import socket
 # ROBOT_ARM Variables
 import time
 
-ROBOT_HOST = '10.1.10.118'  # The server's hostname or IP address
+ROBOT_HOST = '192.168.3.20'  # The server's hostname or IP address
 CONFIG_PORT = 10005  # The port used by the server
 POS_PORT = 10003
 
@@ -35,13 +35,16 @@ def config_robot_arm():
 
 config_robot_arm()
 
-pos_x = 300.0
-pos_y = 400.0
-pos_z = 400.0
-pos_a = -180.0
+pos_x = 578.520
+pos_y = 39.59
+pos_z = 520.0
+pos_a = 180.0
 pos_b = 0.0
-pos_c = 180.0
-for _ in range(10):
+pos_c = -30.0
+switch = False
+
+
+def move():
     pos = str.encode(
         "(" + str(pos_x) + "," + str(pos_y) + "," + str(pos_z) + "," + str(pos_a) + "," + str(pos_b) + "," + str(
             pos_c) + ")(7,0)")
@@ -50,10 +53,31 @@ for _ in range(10):
         s.sendall(pos)
         data = s.recv(1024)
     print('Received', repr(data))
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((ROBOT_HOST, POS_PORT))
-        s.sendall(b'10')
-        data = s.recv(1024)
-    print('Received', repr(data))
-    pos_x += 10.0
-    time.sleep(1)
+
+
+for i in range(10):
+    for j in range(10):
+        move()
+        time.sleep(.5)
+        if switch:
+            pos_y += 15
+        else:
+            pos_y -= 15
+    switch = ~switch
+    pos_x += 20
+
+# for i in range(10):
+#     pos = str.encode(
+#         "(" + str(pos_x) + "," + str(pos_y) + "," + str(pos_z) + "," + str(pos_a) + "," + str(pos_b) + "," + str(
+#             pos_c) + ")(7,0)")
+#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#         s.connect((ROBOT_HOST, POS_PORT))
+#         s.sendall(pos)
+#         data = s.recv(1024)
+#     print('Received', repr(data))
+#     if (i % 2 == 0):
+#         pos_z += 100.0
+#     else:
+#         pos_z -= 100.0
+#
+#     time.sleep(.5)
